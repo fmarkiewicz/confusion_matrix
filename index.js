19b8adb35c9911e52003d140e7087935900f8273
fmarkiewicz <markiewiczfryderyk@gmail.com>
2018-04-01 18:13:17 +0200

    Finished

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const BASIC_CALCULATIONS = ['Accuracy', 'Overall error rate', 'False negative rate',
-    'False positive rate', 'Sensitivity', 'Specifity'];
-
 var matrix = [],
     matrix_container = $('.matrix-container');
 
@@ -8,10 +5,24 @@ function createInput(x, y) {
     return $('<input>').attr('type', 'number').attr('min', '0').attr('x', x).attr('y', y).addClass('form-control');
 };
 
+function createTRowWithName(name = '', className = '', rowColor = '', fieldColor = '') {
+    let _tr = $('<tr>').addClass(rowColor).addClass(className),
+        _th = $('<th>').attr('scope', 'row').text(name).addClass(fieldColor).appendTo(_tr);
+    return _tr;
+};
+
+function createTdWithValue(value) {
+    return $('<td>').text(formatCalc(value));
+}
+
+function formatCalc(num) {
+    return num.toFixed(2);
+};
+
 function makeTHead(k) {
     let _thead = $('<thead>').addClass('thead-dark'),
         _tr = $('<tr>');
-        $('<th>').attr('scope', 'col').text('#').appendTo(_tr);
+        $('<th>').attr('scope', 'col').text('Pred\\Real').appendTo(_tr);
     for(let i = 1; i <= k; i++) {
         $('<th>').attr('scope', 'col').text(i).appendTo(_tr);
     }
@@ -24,8 +35,7 @@ function makeTBody(k) {
     let _tbody = $('<tbody>');
 
     for(let i = 1; i <= k; i++) {
-        let _tr = $('<tr>');
-        $('<th>').attr('scope', 'row').addClass('table-dark').text(i).appendTo(_tr);
+        let _tr = createTRowWithName(i, '', '', '');
 
         for(let j = 1; j <= k; j++) {
             let _td = $('<td>');
@@ -43,21 +53,20 @@ function clearMatrix() {
 };
 
 function applyListener(k) {
-    $('.matrix-container input').change( function() {
+    $('.matrix-container input').keyup( function() {
         let _this = $(this),
             _x = _this.attr('x')*1,
             _y = _this.attr('y')*1;
 
         matrix[_y][_x] = _this.val()*1;
 
-        console.table(matrix);
+        calcRest(k);
         calcAcurracyAndOER(k);
     });
 };
 
 function calcAcurracyAndOER(k) {
-    $('.accuracy').html('');
-    $('.OER').html('');
+    $('.accuracy, .OER').remove();
 
     let _TP_TN = 0,
         _FN_FP = 0,
@@ -72,15 +81,63 @@ function calcAcurracyAndOER(k) {
         }
     }
 
-    let _tr_OER = $('<tr>').addClass('table-primary').addClass('OER');
-    $('<th>').attr('scope', 'row').text('Overall error rate').appendTo(_tr_OER);
-    $('<td>').text(_FN_FP / _N).appendTo(_tr_OER);
+    let _tr_OER = createTRowWithName('Overall error rate', 'OER', 'table-primary');
+    createTdWithValue(_FN_FP / _N).appendTo(_tr_OER);
     _tr_OER.appendTo(matrix_container);
 
-    let _tr_accuracy = $('<tr>').addClass('table-primary').addClass('accuracy');
-    $('<th>').attr('scope', 'row').text('Accuracy').appendTo(_tr_accuracy);
-    $('<td>').text(_TP_TN / _N).appendTo(_tr_accuracy);
+    let _tr_accuracy = createTRowWithName('Accuracy', 'accuracy', 'table-primary');
+    createTdWithValue(_TP_TN / _N).appendTo(_tr_accuracy);
     _tr_accuracy.appendTo(matrix_container);
+};
+
+function calcRest(k) {
+    // i -> class
+    // m -> row positin of matrix
+    // n -> column position o matrix
+    
+    $('.FNR, .FPR, .sens, .spec').remove();
+
+    let _tr_FN = createTRowWithName("False negative rate", "FNR", "table-success", ""),
+        _tr_FP = createTRowWithName("False positive rate", "FPR", "table-success", ""),
+        _tr_sensitivity = createTRowWithName("Sensitivity", "sens", "table-success", ""),
+        _tr_specifity = createTRowWithName("Specifity", "spec", "table-success", "");
+
+    // for every class
+    for(let i = 0; i < k; i++) {
+        let _TP = matrix[i][i],
+            _TN = 0,
+            _FP = 0,
+            _FN = 0;
+        //calculate from matrix
+        for(let m = 0; m < k; m++) {
+            for (let n = 0; n < k; n++) {
+
+                if (m === n && m !== i) {
+                    _TN += matrix[m][n];
+                }
+
+                if (n === i && m !== i) {
+                    _FN += matrix[m][n];
+                }
+
+                if (n !== i && m !== n) {
+                    _FP += matrix[m][n];
+                }
+
+            }
+        }
+
+        createTdWithValue(_FN/(_FN+_TN)).appendTo(_tr_FN);
+        createTdWithValue(_FP/(_FP+_TP)).appendTo(_tr_FP);
+        createTdWithValue(_TP/(_TP+_FN)).appendTo(_tr_sensitivity);
+        createTdWithValue(_TN/(_TN+_FP)).appendTo(_tr_specifity);
+
+        _tr_FN.appendTo(matrix_container);
+        _tr_FP.appendTo(matrix_container);
+        _tr_sensitivity.appendTo(matrix_container);
+        _tr_specifity.appendTo(matrix_container);
+
+    }        
 };
 
 $('.k-var').change( function() {
@@ -93,12 +150,10 @@ $('.k-var').change( function() {
     clearMatrix();
 
     matrix = Array(k).fill(null).map(() => Array(k).fill(0));
-    console.table(matrix);
 
     makeTHead(k);
     
     makeTBody(k);
     applyListener(k);
 
-    
 });
